7c16042d649f05ad4589a553a692f5220c9faf47
olga <plotnica@yahoo.com>
2019-01-31 22:34:52 +0200

    gulp bug fixing

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,50 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+// gulp.task('sass', function() {
+//     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+//         .pipe(sass())
+//         .pipe(gulp.dest("src/css"))
+//        // .pipe(browserSync.stream());
+       
+// });
+gulp.task(sassCompile)
+function sassCompile(){
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
-
-// Move the javascript files into our /src/js folder
-gulp.task('js', function() {
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream())
+}
+gulp.task(jsBundel)
+function jsBundel(){
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
-        .pipe(browserSync.stream());
-});
+           .pipe(gulp.dest("src/js"))
+           .pipe(browserSync.stream());
+}
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+// // Move the javascript files into our /src/js folder
+// gulp.task('js', function() {
+//     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+//         .pipe(gulp.dest("src/js"))
+//         .pipe(browserSync.stream());
+// });
 
-    browserSync.init({
-        server: "./src"  
-    });
+// // Static Server + watching scss/html files
+// gulp.task('serve', ['sass'], function() {
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+//     browserSync.init({
+//         server: "./src"  
+//     });
 
-gulp.task('default', ['js','serve']);
+//     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+//     gulp.watch("src/*.html").on('change', browserSync.reload);
+// });
+
+// gulp.task('default', ['js','serve']);
+
+gulp.task('default', gulp.parallel(sassCompile,jsBundel));
+
+
+
