fcfe1b63c12b2efb09885462d52d8511daef8347
olga <plotnica@yahoo.com>
2019-02-03 21:38:52 +0200

    gulp autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const gulp = require('gulp'),
+browserSync = require('browser-sync').create(),
+sass = require('gulp-sass'),
+postcss = require("gulp-postcss"),
+autoprefixer = require("autoprefixer")
+//autoprefixer = require('gulp-autoprefixer')
 
 function styles(){
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
     .pipe(sass())
+    .pipe(postcss([autoprefixer(
+        {
+               browsers: ['last 2 versions'],
+                cascade: false
+             }
+    )]))
+//    .pipe(autoprefixer({
+//     browsers: ['last 2 versions'],
+//     cascade: false
+//      }))
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.reload({stream:true}))
 }
